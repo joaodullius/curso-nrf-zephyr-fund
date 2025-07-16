3caac21b4bb52b384fdf2846b9df894bc974362c
João Dullius <joaodullius@bpmrep.com.br>
2025-07-15 21:01:30 -0300

    Refactor message queue implementation to use data_sample struct and remove unused BMP280 files

--- 31-thread_msgq/src/main.c
+++ 31-thread_msgq/src/main.c
@@ -4,27 +4,31 @@
 
 LOG_MODULE_REGISTER(prod_cons_msgq, LOG_LEVEL_INF);
 
-K_MSGQ_DEFINE(data_q, sizeof(uint32_t), 10, 4);
+struct data_sample {
+    uint32_t data;
+};
+
+K_MSGQ_DEFINE(data_q, sizeof(struct data_sample), 10, 4);
 
 void producer_thread(void)
 {
-    uint32_t data;
+    struct data_sample sample;
 
     while (1) {
-        data = sys_rand32_get();
-        LOG_INF("Produced data: %u", data);
-        k_msgq_put(&data_q, &data, K_FOREVER);
+        sample.data = sys_rand32_get();
+        LOG_INF("Produced data: %u", sample.data);
+        k_msgq_put(&data_q, &sample, K_FOREVER);
         k_msleep(1000);
     }
 }
 
 void consumer_thread(void)
 {
-    uint32_t data;
+    struct data_sample sample;
 
     while (1) {
-        k_msgq_get(&data_q, &data, K_FOREVER);
-        LOG_INF("Consumed data: %u", data);
+        k_msgq_get(&data_q, &sample, K_FOREVER);
+        LOG_INF("Consumed data: %u", sample.data);
     }
 }
 
