6ea4bc787c2ba1ae60e407de86aef2ae46c1e779
João Dullius <joaodullius@bpmrep.com.br>
2025-07-20 16:48:12 -0300

    Add timestamp to data_sample structure and log consumed data with timestamp

--- 31-thread_msgq/src/main.c
+++ 31-thread_msgq/src/main.c
@@ -5,11 +5,14 @@
 LOG_MODULE_REGISTER(prod_cons_msgq, LOG_LEVEL_INF);
 
 struct data_sample {
+    uint32_t timestamp;
     uint32_t data;
 };
 
 #define MSGQ_SIZE 10
 K_MSGQ_DEFINE(data_q, sizeof(struct data_sample), MSGQ_SIZE, 4);
+
+int timestamp_counter = 0;
 
 void producer_thread(void)
 {
@@ -17,6 +20,7 @@ void producer_thread(void)
 
     while (1) {
         sample.data = sys_rand32_get();
+        sample.timestamp = timestamp_counter++;
         LOG_INF("Produced data: %u", sample.data);
         k_msgq_put(&data_q, &sample, K_NO_WAIT);
         k_msleep(1000);
@@ -29,7 +33,7 @@ void consumer_thread(void)
 
     while (1) {
         k_msgq_get(&data_q, &sample, K_FOREVER);
-        LOG_INF("Consumed data: %u", sample.data);
+        LOG_INF("%d | Consumed data: %u", sample.timestamp, sample.data);
     }
 }
 
