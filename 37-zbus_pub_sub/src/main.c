8aa4e81d5104d0b09af1015ddcf69a0887b03211
João Dullius <joaodullius@bpmrep.com.br>
2025-07-26 19:44:08 -0300

    Fix subscriber definition and improve error handling in consumer thread

--- 37-zbus_pub_sub/src/main.c
+++ 37-zbus_pub_sub/src/main.c
@@ -10,13 +10,15 @@ struct data_sample {
     uint32_t data;
 };
 
+/* Define the subscriber correctly */
 ZBUS_SUBSCRIBER_DEFINE(cons_sub, 4);
 
+/* Define the channel with the subscriber */
 ZBUS_CHAN_DEFINE(data_chan,
                  struct data_sample,
                  NULL,
                  NULL,
-                 ZBUS_OBSERVERS(&cons_sub),
+                 ZBUS_OBSERVERS(cons_sub),  /* Remove the & here */
                  ZBUS_MSG_INIT(.timestamp = 0, .data = 0));
 
 int timestamp_counter = 0;
@@ -42,9 +44,18 @@ void consumer_thread(void)
     struct data_sample sample;
 
     while (1) {
-        zbus_sub_wait(&cons_sub, &chan, K_FOREVER);
-        zbus_chan_read(&data_chan, &sample, K_NO_WAIT);
-        LOG_INF("%d | Consumed data: %u", sample.timestamp, sample.data);
+        int ret = zbus_sub_wait(&cons_sub, &chan, K_FOREVER);
+        if (ret == 0) {
+            if (chan == &data_chan) {
+                if (zbus_chan_read(&data_chan, &sample, K_NO_WAIT) == 0) {
+                    LOG_INF("%u | Consumed data: %u", sample.timestamp, sample.data);
+                } else {
+                    LOG_ERR("Failed to read from channel");
+                }
+            }
+        } else {
+            LOG_ERR("zbus_sub_wait failed with error: %d", ret);
+        }
     }
 }
 
@@ -57,6 +68,5 @@ K_THREAD_DEFINE(cons_tid, STACK_SIZE, consumer_thread, NULL, NULL, NULL, PRIORIT
 int main(void)
 {
     LOG_INF("Producer-Consumer example using Zbus");
-
     return 0;
 }
