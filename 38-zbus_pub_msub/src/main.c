5cba5797b9baca54211c69a82fb3c859c2e3de45
João Dullius <joaodullius@bpmrep.com.br>
2025-07-26 20:20:52 -0300

    Add zbus message subscriber example

---
+++ 38-zbus_pub_msub/src/main.c
@@ -0,0 +1,65 @@
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/random/random.h>
+#include <zephyr/zbus/zbus.h>
+
+LOG_MODULE_REGISTER(prod_cons_zbus, LOG_LEVEL_INF);
+
+struct data_sample {
+    uint32_t timestamp;
+    uint32_t data;
+};
+
+/* Define a message subscriber */
+ZBUS_MSG_SUBSCRIBER_DEFINE(cons_msub, 4);
+
+/* Define the channel with the subscriber */
+ZBUS_CHAN_DEFINE(data_chan,
+                 struct data_sample,
+                 NULL,
+                 NULL,
+                 ZBUS_OBSERVERS(cons_msub),
+                 ZBUS_MSG_INIT(.timestamp = 0, .data = 0));
+
+int timestamp_counter = 0;
+
+void producer_thread(void)
+{
+    struct data_sample sample;
+
+    while (1) {
+        sample.data = sys_rand32_get();
+        sample.timestamp = timestamp_counter++;
+
+        zbus_chan_pub(&data_chan, &sample, K_NO_WAIT);
+        LOG_INF("%d | Produced data: %u", sample.timestamp, sample.data);
+
+        k_msleep(1000);
+    }
+}
+
+void consumer_thread(void)
+{
+    struct data_sample sample;
+
+    while (1) {
+        int ret = zbus_msg_sub_get(&cons_msub, &sample, K_FOREVER);
+        if (ret == 0) {
+            LOG_INF("%u | Consumed data: %u", sample.timestamp, sample.data);
+        } else {
+            LOG_ERR("zbus_msg_sub_get failed: %d", ret);
+        }
+    }
+}
+
+#define STACK_SIZE 1024
+#define PRIORITY 5
+
+K_THREAD_DEFINE(prod_tid, STACK_SIZE, producer_thread, NULL, NULL, NULL, PRIORITY, 0, 0);
+K_THREAD_DEFINE(cons_tid, STACK_SIZE, consumer_thread, NULL, NULL, NULL, PRIORITY, 0, 0);
+
+int main(void)
+{
+    LOG_INF("Producer-Consumer example using Zbus");
+    return 0;
+}
