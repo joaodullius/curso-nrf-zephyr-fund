8c5e4acda74482415565bcb2f08bc472234f28bb
João Dullius <joaodullius@bpmrep.com.br>
2025-08-15 10:34:29 -0300

    Refactor sensor data handling by removing message queue and improving error logging

--- 42-bmp180_beacon/src/main.c
+++ 42-bmp180_beacon/src/main.c
@@ -13,12 +13,6 @@ LOG_MODULE_REGISTER(bmp180_beacon, LOG_LEVEL_INF);
 
 const struct device *bmp180 = DEVICE_DT_GET_ONE(bosch_bmp180);
 
-#define MSGQ_SIZE 10
-struct bmp180_msg {
-    struct sensor_value temp;
-};
-K_MSGQ_DEFINE(bmp180_q, sizeof(struct bmp180_msg), MSGQ_SIZE, 4);
-
 /* Manufacturer data: 0xFFFF ID + 2 bytes temperature */
 static uint8_t mfg_data[4] = { 0xFF, 0xFF, 0x00, 0x00 };
 
@@ -29,29 +23,42 @@ static const struct bt_data ad[] = {
 
 /* Set Scan Response data */
 static const struct bt_data sd[] = {
-	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+    BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
 };
-
 
 static void sensor_work_handler(struct k_work *work)
 {
-    struct bmp180_msg msg;
-    int ret;
+    struct sensor_value temp;
+    int err;
 
     LOG_INF("Fetching sensor data...");
 
     if (sensor_sample_fetch(bmp180) != 0) {
         LOG_ERR("sensor_sample_fetch failed");
-    } else if (sensor_channel_get(bmp180, SENSOR_CHAN_DIE_TEMP, &msg.temp) != 0) {
+        return;
+    }
+    if (sensor_channel_get(bmp180, SENSOR_CHAN_DIE_TEMP, &temp) != 0) {
         LOG_ERR("sensor_channel_get failed");
+        return;
+    }
+
+    /* Converte sensor_value -> centi-°C (s16), truncando */
+    int32_t centi = temp.val1 * 100 + temp.val2 / 10000;
+    int16_t t_centi = (int16_t)centi;
+
+    memcpy(&mfg_data[2], &t_centi, sizeof(t_centi));
+
+    LOG_HEXDUMP_INF(mfg_data, sizeof(mfg_data), "Manufacturer Data");
+
+    err = bt_le_adv_update_data(ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+    if (err) {
+        LOG_ERR("Advertising data update failed (%d)", err);
     } else {
-        ret = k_msgq_put(&bmp180_q, &msg, K_NO_WAIT);
-        if (ret != 0) {
-            LOG_WRN("bmp180_q full, dropping sample Temp: %0.2f C",
-            sensor_value_to_double(&msg.temp));
-        }
+        LOG_INF("Updated Temp: %0.2f C in advertising data",
+                sensor_value_to_double(&temp));
     }
 }
+
 K_WORK_DEFINE(sensor_work, sensor_work_handler);
 
 static void sensor_timer_handler(struct k_timer *timer)
@@ -61,68 +68,32 @@ static void sensor_timer_handler(struct k_timer *timer)
 
 K_TIMER_DEFINE(sensor_timer, sensor_timer_handler, NULL);
 
-void adv_thread(void)
-{
-    struct bmp180_msg msg;
-    int err;
-
-    while (1) {
-        k_msgq_get(&bmp180_q, &msg, K_FOREVER);
-        LOG_INF("Got Message Queue data: Temp: %0.2f C",
-                sensor_value_to_double(&msg.temp));
-
-        /* Converte sensor_value -> centi-°C (s16), truncando */
-        int32_t centi = msg.temp.val1 * 100 + msg.temp.val2 / 10000;
-        int16_t t_centi = (int16_t)centi;
-
-        memcpy(&mfg_data[2], &t_centi, sizeof(t_centi));
-
-        LOG_HEXDUMP_INF(mfg_data, sizeof(mfg_data), "Manufacturer Data");
-
-
-        err = bt_le_adv_update_data(ad, ARRAY_SIZE(ad),sd, ARRAY_SIZE(sd));
-        if (err) {
-            LOG_ERR("Advertising data update failed (%d)", err);
-        } else {
-            LOG_INF("Updated Temp: %0.2f C in advertising data",
-                    sensor_value_to_double(&msg.temp));
-        }
-    }
-}
-
-#define STACK_SIZE 1024
-#define PRIORITY 5
-
-K_THREAD_DEFINE(adv_tid, STACK_SIZE, adv_thread, NULL, NULL, NULL, PRIORITY, 0, 0);
-
-
 static void bt_ready(int err)
 {
-	char addr_s[BT_ADDR_LE_STR_LEN];
-	bt_addr_le_t addr = {0};
-	size_t count = 1;
+    char addr_s[BT_ADDR_LE_STR_LEN];
+    bt_addr_le_t addr = {0};
+    size_t count = 1;
 
-	if (err) {
-		printk("Bluetooth init failed (err %d)\n", err);
-		return;
-	}
+    if (err) {
+        printk("Bluetooth init failed (err %d)\n", err);
+        return;
+    }
 
-	printk("Bluetooth initialized\n");
+    printk("Bluetooth initialized\n");
 
-	/* Start advertising */
-	err = bt_le_adv_start(BT_LE_ADV_NCONN_IDENTITY, ad, ARRAY_SIZE(ad),
-			      sd, ARRAY_SIZE(sd));
-	if (err) {
-		printk("Advertising failed to start (err %d)\n", err);
-		return;
-	}
+    /* Start advertising */
+    err = bt_le_adv_start(BT_LE_ADV_NCONN_IDENTITY, ad, ARRAY_SIZE(ad),
+                          sd, ARRAY_SIZE(sd));
+    if (err) {
+        printk("Advertising failed to start (err %d)\n", err);
+        return;
+    }
 
-	bt_id_get(&addr, &count);
-	bt_addr_le_to_str(&addr, addr_s, sizeof(addr_s));
+    bt_id_get(&addr, &count);
+    bt_addr_le_to_str(&addr, addr_s, sizeof(addr_s));
 
-	printk("Beacon started, advertising as %s\n", addr_s);
+    printk("Beacon started, advertising as %s\n", addr_s);
 }
-
 
 int main(void)
 {
